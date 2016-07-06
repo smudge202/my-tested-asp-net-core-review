5edbfa2786a34911024cc9e21cdffdc9a3c83258
Tommy Long <tommy.long@gilmond.com>
2016-07-06 11:27:03 +0100

    switched to rtm implementation

---
+++ src/Website/Controllers/MyController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website.Controllers
+{
+	public class MyController
+	{
+		public IActionResult Fail(int dummy) => null;
+
+		public IActionResult Suceed(int id) => null;
+	}
+}
---
+++ tests/Website.Tests.Normal/MyControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Website.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace Website.Tests.Normal
+{
+	public class MyControllerTests
+	{
+		[Fact]
+		public async Task MyControllerShouldHaveRouteForActionWithId()
+		{
+			var uri = "/My/Suceed/1";
+			var builder = new WebHostBuilder().UseStartup(typeof(Startup));
+			using (var client = new TestServer(builder).CreateClient())
+			{
+				var response = await client.PostAsync(uri, null);
+				response.IsSuccessStatusCode.Should().BeTrue(
+					because: $"{nameof(MyController)}.{nameof(MyController.Suceed)} should be reachable on '{uri}'.");
+			}
+		}
+	}
+}
