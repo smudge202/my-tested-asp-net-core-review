27d34d4d6e2bcbf7401eb192b7c376c1a87134b7
Tommy Long <tommy.long@gilmond.com>
2016-07-06 08:29:22 +0100

    removed rc2 implementations

--- src/Example.WebApplication/Controllers/MyController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Example.WebApplication.Controllers
-{
-	public class MyController : Controller
-	{
-		public IActionResult Action(int dummy) => View();
-	}
-}
